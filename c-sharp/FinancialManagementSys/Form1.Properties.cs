4038c3056c7af9605ac5dac01f861bca37e25a79
Zaldy-develper <kimetsu@PCN-HN2005>
2025-03-26 15:36:16 +0900

    C sharp SQL with Application Interface, OOP, Injection. Ver2

---
+++ c-sharp/FinancialManagementSys/Form1.Properties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1SQL
+{
+    partial class Form1 : Form
+    {
+        // Create instance for the dataGridView :dataGridViewTransact
+        private GridDataViewTransactEventHandler formTransactUIEvent = new GridDataViewTransactEventHandler();
+        // Declare a field for the higher-level service.
+        private readonly FinancialTransactionService _transactionService;
+        private string m_transactionID = "TransactionID";
+
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            // Use the factory to create the service instance.
+            _transactionService = ServiceFactory.CreateFinancialTransactionService();
+            formTransactUIEvent.SetupTransactDataGridView(dataGridTransact);
+            formTransactUIEvent.SetupWindowTabDataGridView(this, tabControlWindow, comboBoxFilter);
+        }
+
+    }
+}
